6e4a73eebd1604f7d618e62da70d1d17a126130c
hjy705 <hjy20007589@gmail.com>
2021-11-29 11:42:39 +0800

    first commit

---
+++ notify.py
@@ -0,0 +1,35 @@
+import requests
+
+class Notify:
+    def __init__(self, token):
+        self.token = token 
+        self.headers = { "Authorization": "Bearer " + self.token}
+    def sendmsg(self, msg):
+        headers = self.headers 
+        payload = {'message': msg}
+        r = requests.post("https://notify-api.line.me/api/notify", headers = headers, params = payload)
+        return r.status_code
+    def localimg(self, msg , pic):
+        headers = self.headers
+        payload = {
+            'message': msg
+        }
+        files = {'imageFile': open(pic, 'rb')}
+        r = requests.post("https://notify-api.line.me/api/notify", headers = headers, params = payload, files = files)
+        return r.status_code
+    def netimg(self, msg , picurl):
+        headers = self.headers
+        payload = {'message': msg , 'imageThumbnail': picurl,'imageFullsize': picurl}
+        r = requests.post("https://notify-api.line.me/api/notify", headers = headers, params = payload)
+        return r.status_code
+    def sticker(self,msg,packageid, stickerID):
+        headers = self.headers
+        payload = {'message': msg ,'stickerPackageId':packageid ,'stickerId': stickerID}
+        r = requests.post("https://notify-api.line.me/api/notify", headers = headers, params = payload)
+        return r.status_code 
+
+myNotify = Notify(token = "1v2ta7DfuYFLrWCiCLDTj7PinmNfD92WL5Wc8l1lvEq")
+# myNotify.sendmsg(msg = "文字測試")
+# res = myNotify.localimg(msg = "我是熊大" , pic = 'image/brown.png')
+# myNotify.netimg(msg = "小熊維尼" , picurl = "https://dvblobcdnjp.azureedge.net//Content/Upload/Popular/Images/2017-07/fa2c3d72-7fad-4ccf-9857-e90af952b956_m.jpg")
+myNotify.sticker(msg ="貼圖測試" , packageid = 446 ,stickerID = 1988)
---
+++ readme.md
@@ -0,0 +1,32 @@
+# **Notify Project**
+
+## **專案說明**
+<br>藉由Line Notify功能，以撰寫物件導向格式撰寫，連線上傳文字訊息、本地端圖片、網路圖片、Line貼圖。
+
+</br>
+
+## **環境說明**
+<br>Windows 10 家用版；
+<br>Python version 3.6.8
+
+</br>
+
+## **檔案說明**
+<br>notify.py 撰寫內容
+<br>image 存放本地端圖片的資料夾
+</br>
+
+## **成果說明**
+<br>使用Line Notify上傳文字訊息
+
+![文字測試](./readmeimg/text_test.jpg)
+
+使用Line Notify上傳文字訊息與本地端圖片
+![文字測試](./readmeimg/local_image_test.jpg)
+
+使用Line Notify上傳文字訊息和網路圖片
+![文字測試](./readmeimg/image_test.jpg)
+
+使用Line Notify上傳文字訊息和貼圖
+![文字測試](./readmeimg/sticker_test.jpg)
+
